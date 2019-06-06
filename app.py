43bfa4d01558553e10c20d8ca8bcd0087303c2ab
swatisalwan <salwan.swati1687@gmail.com>
2019-06-06 10:31:38 +0530

    final

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import wikipedia
 app= Flask(__name__)
 
 
-@app.route("/", methods =['GET','POST'])
+@app.route("/login", methods =['GET','POST'])
 def login():
     if request.method == 'GET':
         return render_template("login.html")
